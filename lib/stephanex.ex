b4565a1812acd160f090f0eaca91646b24f4dc6e
alde103 <aldebaran_103@hotmail.com>
2025-07-18 11:48:43 -0600

    first commit

---
+++ lib/stephanex.ex
@@ -0,0 +1,18 @@
+defmodule Stephanex do
+  @moduledoc """
+  Documentation for `Stephanex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stephanex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stephanex_test.exs
@@ -0,0 +1,8 @@
+defmodule StephanexTest do
+  use ExUnit.Case
+  doctest Stephanex
+
+  test "greets the world" do
+    assert Stephanex.hello() == :world
+  end
+end
